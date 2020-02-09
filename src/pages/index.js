a31759763a4d2a96d16c2b1da402dc4f00684ce6
Tim Mathis <>
2020-02-09 10:19:04 -0600

    Minor style improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,15 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>
-      <span>T</span>
-      <span>I</span>
-      <span>M</span>
+    <h1 className="hero-text">
+      <span className="hero-text__letter">T</span>
+      <span className="hero-text__letter">I</span>
+      <span className="hero-text__letter">M</span>
     </h1>
-    <h2>
-      <span>Developer</span>
-      <span>Designer</span>
-      <span>Mentor</span>
+    <h2 className="tagline">
+      <span className="tagline__word">Developer</span>
+      <span className="tagline__word">Designer</span>
+      <span className="tagline__word">Mentor</span>
     </h2>
   </Layout>
 )
